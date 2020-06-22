15c9dfa53ee72364b58824c717b89d76ece1f843
EC2 Default User <ec2-user@ip-172-31-4-98.ap-northeast-1.compute.internal>
2020-06-22 01:35:53 +0000

    create form_mvc

---
+++ resources/views/admin_blog/form.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="{{ asset('/css/blog.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>ブログ記事投稿フォーム</title>
+</head>
+
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <h2 class="border-bottom py-2 mb-4">ブログ記事投稿・編集</h2>
+
+            <form method="POST">
+                <div class="form-group">
+                    <label>日付</label>
+                    <input class="form-control w-auto" name="post_date" size="20" value="" placeholder="日付を入力して下さい。">
+                </div>
+
+                <div class="form-group">
+                    <label>タイトル</label>
+                    <input class="form-control" name="title" value="" placeholder="タイトルを入力して下さい。">
+                </div>
+
+                <div class="form-group">
+                    <label>本文</label>
+                    <textarea class="form-control" rows="15" name="body" placeholder="本文を入力してください。"></textarea>
+                </div>
+
+                <input type="submit" class="btn btn-primary btn-sm" value="送信">
+                {{ csrf_field() }}
+            </form>
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('admin/form', 'AdminBlogController@form')->name('admin_form');
